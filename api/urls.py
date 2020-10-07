f691935fd8c24475c68a594cf504631400fe7100
sadiqsyeds <sadiq446@gmail.com>
2020-10-07 21:55:41 +0530

    first commit

---
+++ api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from rest_framework.authtoken import views
+from api import views
+
+urlpatterns = [
+    path('', views.home, name='apihome'),
+    path('category/', include('api.category.urls')),
+    path('product/', include('api.product.urls'))
+]
---
+++ api/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+def home(request):
+    return JsonResponse({
+        'name' : 'Django React Course',
+        'Tutor' : 'Hitest'
+    })
